3ffc306cbf9715ce871d40caaadf6b3dfeea255c
ThuyLinh nekk <dothuylinh_t65@hus.edu.vn>
2024-01-22 14:19:09 +0700

    next js hello world

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,9 @@
 import './bootstrap';
 import React from 'react';
 
-function App() {
-  return <h1>Hello, React!</h1>;
-}
+// function App() {
+//   return <h1>Hello, React!</h1>;
+// }
 
 export default App;
 
