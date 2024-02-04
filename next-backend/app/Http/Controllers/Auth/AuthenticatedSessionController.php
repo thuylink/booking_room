6e81240f173fa2ae27a56f6267c10a909ec79461
ThuyLinh nekk <dothuylinh_t65@hus.edu.vn>
2024-02-04 17:12:10 +0700

    nextjs

---
+++ next-backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function getLoginInfo()
+    {
+        $message = urlencode('Please log in to verify your email.');
+        $redirectUrl = "http://localhost:3000/login?message=$message";
+        Log::debug('Verification URL: chưa verify');
+        echo "<script>
+            setTimeout(function() {
+                alert('Please login and click once again on the email address link you provided during registration to verify your email.');
+                window.location.href = '$redirectUrl';
+            }, 3000); // 5 seconds delay
+        </script>";
+        
+        exit();
+    }
+    public function store(LoginRequest $request): Response
+    {
+        $request->authenticate();
+
+        $request->session()->regenerate();
+
+        return response()->noContent();
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): Response
+    {
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return response()->noContent();
+    }
+}
